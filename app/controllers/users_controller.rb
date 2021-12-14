1ad32bd95347eb1fa9e06d8e1f8e008b420df39b
your_github_username <ammar.haseeb26@gmail.com>
2021-12-14 16:58:42 +0500

    user deletion, etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles= @user.articles.paginate(page: params[:page], per_page: 5)
@@ -35,6 +37,14 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+
+    def destroy
+      @user.destroy
+      session[:user_id] = nil
+      flash[:notice] = "Account and all associated articles are successfully deleted"
+      redirect_to articles_path
+    end
+
   end
 
   private
@@ -45,6 +55,13 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
   end
 
 end
